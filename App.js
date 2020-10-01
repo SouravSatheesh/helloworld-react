dc643f8f760b89f17852847dd677bcfb7b2451ec
Sourav Satheesh <56963819+SouravSatheesh@users.noreply.github.com>
2020-10-01 14:48:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,29 @@ class App extends Component {
     return (
       <View style={styles.view}>
         <Text style={styles.text}>Hello World !</Text>
+        <Text> Just one hello world is not enough</Text>
       </View>
+      <div className="jumbotron">
+	  			<div className="logo">Jomato</div>
+	  			<h1>Find the best Restaurants in your city</h1>
+				<form className="searchBar"  onSubmit={getSearch}>
+					<div>
+						<select id="select_id" className="city form-control" onChange={updateID}>
+		  					<option value="0" hidden>Select City</option>
+		  					<option value="4">Bengaluru</option>
+		  					<option value="3" >Mumbai</option>
+		  					<option value="1">Delhi</option>
+		  					<option value="6">Hyderabad</option>
+		  					<option value="5">Pune</option>
+		  					<option value="11290">Trivandrum</option>
+		  					<option value="7">Chennai</option>
+		  					<option value="11">Ahmedabad</option>				  
+		  				</select>
+						<input id="getText" type="text" className="restaurant" placeholder="Search for Restaurant" onChange={updateSearch}/>
+		  				<button id = "getMessage" className = "search-btn">Search</button>
+					</div>
+				</form>
+	  		</div>
     );  
   }
 }
